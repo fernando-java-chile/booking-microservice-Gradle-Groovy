591f974c0bc16a66b643eacc0234489044603366
fernando-java-chile <fernando.java.chile@gmail.com>
2025-02-10 20:03:26 -0300

    Subiendo proyecto Spring Boot Booking Microservice

---
+++ src/main/groovy/com/ImplementandoDiscoveryService/booking_microservice/BookingMicroserviceApplication.groovy
@@ -0,0 +1,13 @@
+package com.ImplementandoDiscoveryService.booking_microservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BookingMicroserviceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BookingMicroserviceApplication, args)
+	}
+
+}
